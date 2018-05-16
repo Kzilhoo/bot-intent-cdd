b4f860efe9d4cf41a2150423044d8575c63520d6
Adrien EXCOFFIER <adrien.excoffier@epsi.fr>
2018-05-16 13:06:13 +0200

    First commit

---
+++ src/cdd.js
@@ -0,0 +1,5 @@
+export const cdd = [
+    "Vous êtes à la recherche d'un contrat à durée déterminée (CDD) ? Rendez-vous sur https://candidat.pole-emploi.fr/offres/recherche?offresPartenaires=true&rayon=10&tri=0&typeContrat=CDD (et n'oubliez pas de vous connecter à votre espace personneL)",
+    "Pour toutes vos recherches de CDD, faites un tour sur https://candidat.pole-emploi.fr/offres/recherche?offresPartenaires=true&rayon=10&tri=0&typeContrat=CDD",
+    "Renseignez-vos critères de recherche de CDD et trouvez un travail qui vous correspond en quelques clics sur https://candidat.pole-emploi.fr/offres/recherche?offresPartenaires=true&rayon=10&tri=0&typeContrat=CDD",
+]
